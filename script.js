f9bdb00cd66c6b75ba3c7509befe225070de6f01
Anurag sharma <anuragsharma140897@gmail.com>
2024-03-02 21:14:33 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,56 @@
+// Menu links
+let getAbout = document.getElementById("getAbout");
+let getResume = document.getElementById("getResume");
+let getContact = document.getElementById("getContact");
+
+// Sections
+let about = document.getElementById("about");
+let resume = document.getElementById("resume");
+let contact = document.getElementById("contact");
+
+function removeClass() {
+    // Links
+    getAbout.classList.remove('selected');
+    getResume.classList.remove('selected');
+    getContact.classList.remove('selected');
+    // Sections
+    about.classList.remove('view');
+    resume.classList.remove('view');
+    contact.classList.remove('view');
+}
+
+getAbout.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        about.classList.add('view');
+        getAbout.classList.add('selected');
+    }
+
+});
+getResume.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        resume.classList.add('view');
+        getResume.classList.add('selected');
+    }
+})
+getContact.addEventListener('click', function (e) {
+    if (window.innerWidth > 1040) {
+        e.preventDefault();
+        removeClass();
+        contact.classList.add('view');
+        getContact.classList.add('selected');
+    }
+})
+
+
+const counter = document.getElementById("counter1");
+async function updateCounter() {
+let response = await fetch("https://aikyrgey7pedtn6ezbu633l4hq0eqgvr.lambda-url.us-east-1.on.aws/"); 
+let data = await response.json();
+counter.innerHTML = `Views: ${data}`;
+console.log(data)
+}
+updateCounter();
